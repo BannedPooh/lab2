fffa026d7e542ec7176625d60cb1afe45b0cdeda
BannedPooh <rongfei@ucsb.edu>
2021-07-07 11:17:46 +0800

    3

--- block.cpp
+++ block.cpp
@@ -4,6 +4,8 @@ int main()
     int w,h;
     std::cout<<"Enter number of rows and columns:"<<std::endl;
     std::cin>>h>>w;
+    while (w!=0 && h !=0)
+    {
     for (int i = 0; i!=h; ++i)
     {
         for (int j = 0; j!= w; ++j)
@@ -11,6 +13,9 @@ int main()
             std::cout<<"X.";
         }
         std::cout<<std::endl;
+    }
+    std::cout<<"Enter number of rows and columns:"<<std::endl;
+    std::cin>>h>>w;
     }
     return 0;
 }
